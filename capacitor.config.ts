ac15e00cdca2ebd025112c543341425f4264eeb3
LuckyEnough2Play <withanyluck@outlook.com>
2025-07-26 10:15:13 -0400

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marbleminds.bubblecontacts',
+  appName: 'Bubble Contacts',
+  webDir: 'dist'
+};
+
+export default config;
